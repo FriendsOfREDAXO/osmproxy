d2732f8ea4e7dafa62a1318b7904f0346bc18977
Thomas Skerbis <thomas.skerbis@mac.com>
2020-12-04 22:18:46 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -3,7 +3,7 @@
 function deleteOSMCacheFiles($dir, $patterns = "*", int $timeout = 86400)
 {
 
-    foreach (glob($dir . "/*" . "{{$patterns}}", GLOB_BRACE) as $f) {
+    foreach (glob($dir . "*" . "{{$patterns}}", GLOB_BRACE) as $f) {
 
         if (is_writable($f) && filemtime($f) < (time() - $timeout))
             unlink($f);
