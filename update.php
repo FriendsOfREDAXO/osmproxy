95db543bac3c9da511beca5e107884ae83f6f4c3
Thomas Skerbis <thomas.skerbis@mac.com>
2021-03-24 12:14:21 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 $addon = rex_addon::get('osmproxy');
 
-if (rex_string::versionCompare($addon->getVersion(), '1.4.0', '<')) {
+if (rex_string::versionCompare($addon->getVersion(), '2.0.0', '<')) {
     rex_dir::delete($addon->getDataPath());
     rex_dir::create($addon->getCachePath());
 }
